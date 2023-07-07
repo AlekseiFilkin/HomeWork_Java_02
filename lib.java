4be73adf9ac98098acfec2c0a978323046f44f6f
Aleksey_Filkin <acid_wind@mail.ru>
2023-07-07 23:58:50 +0700

    task2 solved

--- lib.java
+++ lib.java
@@ -31,7 +31,7 @@ public class lib {
         System.out.print(arr[arr.length-1]);
     }
     
-    public static void printArraeString(String [] arr) {
+    public static void printArrayString(String [] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
